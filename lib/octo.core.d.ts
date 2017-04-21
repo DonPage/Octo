db9f724fa54e7a2678153ba88089500ccfff4192
Donald Page <don.page.work@gmail.com>
2017-04-21 14:45:10 -0400

    added: commonly used commands. untested.

--- lib/octo.core.d.ts
+++ lib/octo.core.d.ts
@@ -1,8 +1,20 @@
 import { WebDriver } from 'selenium-webdriver';
 export declare class Octo {
-    private driver;
     private greeting;
-    constructor(driver: WebDriver);
+    private _core;
+    constructor(...drivers: WebDriver[]);
     go(url: string): Promise<void>;
+    el(selector: string): {
+        _selector: string;
+        click: () => Promise<any>;
+        type: (input: string, throttle?: number) => Promise<void>;
+        getText: () => Promise<string>;
+    };
+    click(selector: string): Promise<void>;
     quit(): Promise<void>;
+    sleep(ms?: number): Promise<void>;
+    type(selector: string, input: string, throttle?: number): Promise<void>;
+    getText(selector: string): Promise<string>;
+    private getElement(selector);
+    private wrapElement(selector);
 }
--- src/octo.core.ts
+++ src/octo.core.ts
@@ -1,18 +1,69 @@
-import { WebDriver } from 'selenium-webdriver';
+import { By, WebDriver, WebElement } from 'selenium-webdriver';
 import { Retry } from './_util';
 
 export class Octo {
   private greeting: string;
+  private _core: WebDriver;
 
-  constructor(private driver: WebDriver) {}
+  constructor(...drivers: WebDriver[]) {
+    // take the first driver out of array.
+    this._core = drivers[0];
+  }
 
-  @Retry()
   public async go(url: string): Promise<void> {
-    await this.driver.get(url);
+    await this._core.get(url);
+    return;
+  }
+
+  public el(selector: string) {
+    return this.wrapElement(selector);
+  }
+
+  @Retry(5)
+  public async click(selector: string): Promise<void> {
+    const element = await this.getElement(selector);
+    await element.click();
+    return;
   }
 
   public async quit(): Promise<void> {
-    await this.driver.quit();
+    await this._core.quit();
     return;
+  }
+
+  public async sleep(ms: number = 50) {
+    await this._core.sleep(ms);
+    return;
+  }
+
+  public async type(selector: string, input: string, throttle: number = 150): Promise<void> {
+      const el = await this.getElement(selector);
+      const keys = input.split('');
+      for (let key of keys) {
+        await this.sleep(throttle + (Math.random() * (10 * 2) + 2));
+        await el.sendKeys(key);
+      }
+      return;
+  }
+
+  public async getText(selector: string): Promise<string> {
+    const el = await this.getElement(selector);
+    return await el.getText();
+  }
+
+  private async getElement(selector: string): Promise<WebElement> {
+    return await this._core.findElement(By.css(selector));
+  }
+
+  private wrapElement(selector: string) {
+    return {
+      _selector: selector,
+      click: async () => {
+        await this.click(selector);
+        return this.wrapElement(selector);
+      },
+      type: async (input: string, throttle = 50) => await this.type(selector, input, throttle),
+      getText: async () => await this.getText(selector)
+    };
   }
 }
